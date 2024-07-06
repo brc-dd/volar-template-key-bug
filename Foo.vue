957d3fb8c8387f7aad2e2d5de3af9dde71c88c74
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-07-06 21:47:53 +0530

    chore: add more cases

--- Foo.vue
+++ Foo.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
+  <template v-if="true" :key=""></template>
   <template v-if="true" :key="{}"></template>
+
+  <div v-if="true" :key=""></div>
   <div v-if="true" :key="{}"></div>
 </template>
